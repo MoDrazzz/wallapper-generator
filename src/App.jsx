6a46cb8f3650880342a71b0b8439f6e09371029f
Grzesio <macbook@MacBook-Pro-Grzesio.local>
2022-11-23 12:09:46 +0100

    Implement getDaysFromMonth() function

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const getDaysInMonth = (year, monthIndex) => Array.from(
+    // Create an array based on array-like object (having length property of last day of provided month).
+    { length: new Date(year, monthIndex + 1, 0).getDate() },
+    // Each element of created array is Date object of next day in the month.
+    (_, arrIndex) => new Date(year, monthIndex, arrIndex + 1)
+  );
+
+  console.log(getDaysInMonth(2023, 4));
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Test!</h1> 
     </div>
   )
 }
