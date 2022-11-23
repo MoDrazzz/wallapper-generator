3c96a094875148d86dbeb52778f00099d19977fb
Grzesio <macbook@MacBook-Pro-Grzesio.local>
2022-11-23 12:55:21 +0100

    Add prettier and tailwind.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css'
+import "./App.css";
 
 const App = () => {
-  const getDaysInMonth = (year, monthIndex) => Array.from(
-    // Create an array based on array-like object (having length property of last day of provided month).
-    { length: new Date(year, monthIndex + 1, 0).getDate() },
-    // Each element of created array is Date object of next day in the month.
-    (_, arrIndex) => new Date(year, monthIndex, arrIndex + 1)
-  );
+  const getDaysInMonth = (year, monthIndex) =>
+    Array.from(
+      // Create an array based on array-like object (having length property of last day of provided month).
+      { length: new Date(year, monthIndex + 1, 0).getDate() },
+      // Each element of created array is Date object of next day in the month.
+      (_, arrIndex) => new Date(year, monthIndex, arrIndex + 1)
+    );
 
   console.log(getDaysInMonth(2023, 4));
 
   return (
     <div className="App">
-      <h1>Test!</h1> 
+      <h1>Test!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
