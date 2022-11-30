811cfacfa750d66d05313609a0e53d2ce52e0ebf
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2022-11-30 19:09:30 +0100

    coderdojo

--- src/components/molecules/Dropzone.jsx
+++ src/components/molecules/Dropzone.jsx
@@ -4,78 +4,74 @@ import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import DropzoneParagraph from "@/components/atoms/DropzoneParagraph";
 import { flushSync } from "react-dom";
-
-import { DataContext } from "@/pages/Root";
-import { useContext, useEffect } from "react";
+import { useDataContext } from "@/components/DataProvider";
 
 const Dropzone = () => {
-  const { handleErrors, isLocked } = useOutletContext();
-  const navigate = useNavigate();
-  const { photo, setPhoto } = useContext(DataContext);
+	const { handleErrors, isLocked } = useOutletContext();
+	const navigate = useNavigate();
+	const { photo, setPhoto } = useDataContext();
 
-  // useEffect(() => {
-  //   if (photo) {
-  //     return navigate("/details");
-  //   }
-  // }, [photo]);
+	// useEffect(() => {
+	//   if (photo) {
+	//     return navigate("/details");
+	//   }
+	// }, [photo]);
 
-  const handleDropzone = (acceptedFiles, rejectedFiles) => {
-    // Check if there is any rejected file. If so, it means that files shouldn't be passed.
-    if (rejectedFiles.length) {
-      // Look for the reason of rejected files. If their count is more than one, handle given error.
-      if (rejectedFiles.length > 1 || acceptedFiles.length) {
-        handleErrors("More than one file was choosen.");
-      } else {
-        handleErrors("Wrong file type.");
-      }
-    } else {
-      // console.log("Passed!", acceptedFiles);
-      flushSync(() => {
-        setPhoto(acceptedFiles[0]);
-      });
-      navigate("/details");
-    }
-  };
+	const handleDropzone = (acceptedFiles, rejectedFiles) => {
+		// Check if there is any rejected file. If so, it means that files shouldn't be passed.
+		if (rejectedFiles.length) {
+			// Look for the reason of rejected files. If their count is more than one, handle given error.
+			if (rejectedFiles.length > 1 || acceptedFiles.length) {
+				handleErrors("More than one file was choosen.");
+			} else {
+				handleErrors("Wrong file type.");
+			}
+		} else {
+			flushSync(() => {
+				setPhoto(acceptedFiles[0]);
+			});
+			navigate("/details");
+		}
+	};
 
-  const {
-    getRootProps,
-    getInputProps,
-    isDragAccept,
-    isDragReject,
-    isFileDialogActive,
-  } = useDropzone({
-    accept: {
-      "image/jpeg": [],
-      "image/png": [],
-    },
-    onDrop: handleDropzone,
-    disabled: isLocked,
-    multiple: false,
-  });
+	const {
+		getRootProps,
+		getInputProps,
+		isDragAccept,
+		isDragReject,
+		isFileDialogActive,
+	} = useDropzone({
+		accept: {
+			"image/jpeg": [],
+			"image/png": [],
+		},
+		onDrop: handleDropzone,
+		disabled: isLocked,
+		multiple: false,
+	});
 
-  return (
-    <div
-      {...getRootProps({
-        className: `bg-dimmedWhite transition-[border-color,_opacity] duration-500 ease-in-out flex py-[40px] flex-1 w-full flex-col items-center justify-center gap-[10px] rounded-[20px] border-2 border-dashed ${
-          isDragAccept
-            ? "border-green-500"
-            : isDragReject
-            ? "border-primary"
-            : isFileDialogActive
-            ? "border-secondary"
-            : "border-lightGray"
-        } ${isLocked ? "cursor-normal opacity-[0.5]" : "cursor-pointer"}`,
-      })}
-    >
-      <input {...getInputProps()} />
-      <FontAwesomeIcon icon={faUpload} className="text-2xl text-lightGray" />
-      <DropzoneParagraph>
-        Drag and drop image
-        <br />
-        or click to browse
-      </DropzoneParagraph>
-    </div>
-  );
+	return (
+		<div
+			{...getRootProps({
+				className: `bg-dimmedWhite transition-[border-color,_opacity] duration-500 ease-in-out flex py-[40px] flex-1 w-full flex-col items-center justify-center gap-[10px] rounded-[20px] border-2 border-dashed ${isDragAccept
+						? "border-green-500"
+						: isDragReject
+							? "border-primary"
+							: isFileDialogActive
+								? "border-secondary"
+								: "border-lightGray"
+					} ${isLocked ? "cursor-normal opacity-[0.5]" : "cursor-pointer"}`,
+			})}
+		>
+			<input {...getInputProps()} />
+			<FontAwesomeIcon icon={faUpload} className="text-2xl text-lightGray" />
+			<DropzoneParagraph>
+				Drag and drop image
+				<br />
+				or click to browse
+			</DropzoneParagraph>
+		</div>
+	);
 };
 
 export default Dropzone;
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -5,59 +5,46 @@ import ContentContainer from "@/components/organisms/ContentContainer";
 import Errors from "@/components/molecules/Errors";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import Stages from "@/components/molecules/Stages";
-// import { DataContext } from "@/App";
-
-export const DataContext = React.createContext();
 
 const Root = () => {
-  const [photo, setPhoto] = useState();
-  const [details, setDetails] = useState();
-  const [errors, setErrors] = useState([]);
-  const [isLocked, setIsLocked] = useState(false);
-  // const { photo, details } = useContext(DataContext);
-  const navigate = useNavigate();
+	const [errors, setErrors] = useState([]);
+	const [isLocked, setIsLocked] = useState(false);
+	const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (!photo) {
-  //     navigate("/");
-  //   } else if (!details) {
-  //     navigate("/details");
-  //   } else if (details) {
-  //     navigate("/download");
-  //   }
-  // }, [photo, details]);
+	console.log("1");
 
-  const handleErrors = (...msg) => {
-    setErrors([...errors, ...msg]);
-    setIsLocked(true);
-    setTimeout(() => {
-      const newErrors = errors;
-      newErrors.shift();
-      setErrors(newErrors);
-      setIsLocked(false);
-    }, 5000);
-  };
+	// useEffect(() => {
+	//   if (!photo) {
+	//     navigate("/");
+	//   } else if (!details) {
+	//     navigate("/details");
+	//   } else if (details) {
+	//     navigate("/download");
+	//   }
+	// }, [photo, details]);
 
-  return (
-    <div className="flex min-h-[100vh] w-[100vw] flex-col items-center justify-center gap-[20px] bg-dimmedWhite p-[10px] sm:gap-[35px] md:gap-[50px]">
-      <Logo />
-      <Title>Wallpaper generator - are you ready to create one?</Title>
-      <ContentContainer>
-        <Stages />
-        <DataContext.Provider
-          value={{
-            photo,
-            setPhoto,
-            details,
-            setDetails,
-          }}
-        >
-          <Outlet context={{ handleErrors, isLocked }} />
-        </DataContext.Provider>
-      </ContentContainer>
-      {errors.length ? <Errors errors={errors} /> : null}
-    </div>
-  );
+	const handleErrors = (...msg) => {
+		setErrors([...errors, ...msg]);
+		setIsLocked(true);
+		setTimeout(() => {
+			const newErrors = errors;
+			newErrors.shift();
+			setErrors(newErrors);
+			setIsLocked(false);
+		}, 5000);
+	};
+
+	return (
+		<div className="flex min-h-[100vh] w-[100vw] flex-col items-center justify-center gap-[20px] bg-dimmedWhite p-[10px] sm:gap-[35px] md:gap-[50px]">
+			<Logo />
+			<Title>Wallpaper generator - are you ready to create one?</Title>
+			<ContentContainer>
+				<Stages />
+				<Outlet context={{ handleErrors, isLocked }} />
+			</ContentContainer>
+			{errors.length ? <Errors errors={errors} /> : null}
+		</div>
+	);
 };
 
 export default Root;
